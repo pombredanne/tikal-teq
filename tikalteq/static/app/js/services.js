ad1ce26f8901acfb4b9c7c2354d9b6781ca535b5
liorb <lior.boord@gmail.com>
2013-10-30 13:49:12 +0200

    restangular configuration moved to main module

--- tikalteq/static/app/js/services.js
+++ tikalteq/static/app/js/services.js
@@ -6,27 +6,11 @@
 TikalTeq.factory('CandidateService', function ($q, Restangular) {
     return {
          list: function () {
-                Restangular.setBaseUrl("../../api/");
-                //adjust to Django Rest API format
-                Restangular.setResponseExtractor(
-                    function(response, operation, what, url, deferred) {
-                        if (operation === "getList") {
-                            var newResponse = response.results;
-                            newResponse._resultmeta = {
-                                "count": response.count,
-                                "next": response.next,
-                                "previous": response.previous
-                            };
-                            return newResponse;
-                        }
-                        return response;
-                    }
-                )
-                var deferred = $q.defer();
-                var candidatesService = Restangular.all('candidates/').getList().then(function(data){
-                   deferred.resolve(data);
-                });
-                return deferred.promise;
+            var deferred = $q.defer();
+            var candidatesService = Restangular.all('candidates/').getList().then(function(data){
+               deferred.resolve(data);
+            });
+            return deferred.promise;
         }
     }
 })
