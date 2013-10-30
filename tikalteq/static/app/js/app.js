9f4ebe76e7bebc84bf28b1bcca8f255a6e5990fb
liorb <lior.boord@gmail.com>
2013-10-30 11:12:30 +0200

    commented code that is not working

--- tikalteq/static/app/js/app.js
+++ tikalteq/static/app/js/app.js
@@ -6,25 +6,25 @@
 /* App Module */
 var TikalTeq = angular.module('TikalTeq', ['ngRoute','restangular', 'ngGrid']).
   config(
-        function(RestangularProvider) {
-            RestangularProvider.setBaseUrl("../../api/");
-            RestangularProvider.setResponseExtractor(
-                function(response, operation, what, url) {
-                    if (operation === "getList") {
-                        // Use results as the return type, and save the result metadata
-                        // in _resultmeta
-                        var newResponse = response.results;
-                        newResponse._resultmeta = {
-                            "count": response.count,
-                            "next": response.next,
-                            "previous": response.previous
-                        };
-                        return newResponse;
-                    }
-                    return response;
-                }
-              )
-        },
+//        function(RestangularProvider) {
+//            RestangularProvider.setBaseUrl("../../api/");
+//            RestangularProvider.setResponseExtractor(
+//                function(response, operation, what, url) {
+//                    if (operation === "getList") {
+//                        // Use results as the return type, and save the result metadata
+//                        // in _resultmeta
+//                        var newResponse = response.results;
+//                        newResponse._resultmeta = {
+//                            "count": response.count,
+//                            "next": response.next,
+//                            "previous": response.previous
+//                        };
+//                        return newResponse;
+//                    }
+//                    return response;
+//                }
+//              )
+//        },
         ['$routeProvider',
         function($routeProvider) {
             $routeProvider.when('/candidates', {
